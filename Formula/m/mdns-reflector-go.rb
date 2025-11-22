12af43bc4a641a86ccd0646ce20e2920c9e7302f
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-22 23:53:27 +0800

    optimize: add Go proxy and build optimizations

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -2,7 +2,6 @@ class MdnsReflectorGo < Formula
   desc "mDNS reflector for forwarding multicast DNS packets between network interfaces"
   homepage "https://github.com/FangTianwd/mdns_reflector_go"
 
-  # For local development/testing - replace with actual release URL when publishing
   url "https://github.com/FangTianwd/mdns_reflector_go/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "1679fc207f8202d3bde7126380a211357ac8a53ba7f0bc2e16af29f8159f45f0"
 
@@ -11,7 +10,9 @@ class MdnsReflectorGo < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", "-o", bin/"mdns-reflector-go", "."
+    # 使用 Go 代理加速依赖下载
+    ENV["GOPROXY"] = "https://goproxy.cn,direct"
+    system "go", "build", "-ldflags", "-s -w", "-o", bin/"mdns-reflector-go", "."
   end
 
   service do
