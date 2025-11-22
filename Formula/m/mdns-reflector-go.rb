4a187dd400fc658b3da0082cbaec6e9608349191
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-23 00:26:04 +0800

    fix: correct sha256 syntax for multi-arch support

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -4,8 +4,11 @@ class MdnsReflectorGo < Formula
 
   # 使用预编译二进制文件 - 无需本地编译，安装速度极快！
   url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.0.2/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-1.0.2.tar.gz"
-  sha256 arm64: "e6dd507e8cb676019f9b6058253fcf7949637706ed2d66c569150df4c87c6e8f",
-         intel: "26386f6e11a0c82710ccb65c4272e066f61c2c45d8c9bffe352a7baa37a35b12"
+  if Hardware::CPU.arm?
+    sha256 "e6dd507e8cb676019f9b6058253fcf7949637706ed2d66c569150df4c87c6e8f"
+  else
+    sha256 "26386f6e11a0c82710ccb65c4272e066f61c2c45d8c9bffe352a7baa37a35b12"
+  end
 
   license "MIT"
 
