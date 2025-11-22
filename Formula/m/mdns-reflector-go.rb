dc514aea0ef7bf76f94bbb8c08321669a6eda149
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-22 23:13:34 +0800

    Update Formula URLs to use correct repository name mdns_reflector_go

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -1,10 +1,10 @@
 class MdnsReflectorGo < Formula
   desc "mDNS reflector for forwarding multicast DNS packets between network interfaces"
-  homepage "https://github.com/FangTianwd/mdns-reflector-go"
+  homepage "https://github.com/FangTianwd/mdns_reflector_go"
 
   # For local development/testing - replace with actual release URL when publishing
-  url "https://github.com/FangTianwd/mdns-reflector-go/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "https://github.com/FangTianwd/mdns_reflector_go/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "67dea9b29717cfa6835e92900de77cf8a3551a05519522c5edb0750f0f3b6b30"
 
   license "MIT"
 
