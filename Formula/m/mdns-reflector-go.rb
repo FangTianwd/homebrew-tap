853b27c4b3590856fb4645e493f40f59740f30c7
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-22 23:01:13 +0800

    Update mdns-reflector-go SHA256 for v1.0.0

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -1,8 +1,8 @@
 class MdnsReflectorGo < Formula
   desc "mDNS reflector for forwarding multicast DNS packets between network interfaces"
   homepage "https://github.com/FangTianwd/mdns-reflector-go"
-  url "https://github.com/FangTianwd/mdns-reflector-go/archive/refs/tags/v#{version}.tar.gz"
-  sha256 "CHANGE_THIS_WITH_ACTUAL_SHA256"
+  url "https://github.com/FangTianwd/mdns-reflector-go/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "MIT"
 
   depends_on "go" => :build
