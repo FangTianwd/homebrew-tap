421147cb3e2fa00ddcf161d5b3cd952269ff366c
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-23 02:03:18 +0800

    fix: update SHA256 checksums for v1.0.4 binary release
    - Update ARM64 SHA256 to 1e30eedf68667cac349018f001bddd278956473665f585859346f4b134ff04f6
    - Update AMD64 SHA256 to 90cc687b471d8ab7afc23edf4efcf10743fdeebf9910952d42586d1e13315a08
    - Ensures checksums match the latest v1.0.4 release artifacts

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -5,9 +5,9 @@ class MdnsReflectorGo < Formula
   # 使用预编译二进制文件 - 无需本地编译，安装速度极快！
   url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.0.4/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-v1.0.4.tar.gz"
   if Hardware::CPU.arm?
-    sha256 "aba7cea65e809155009b99a8ae11b858078a2cb4cad66a2b40e8f7660dedf015"
+    sha256 "1e30eedf68667cac349018f001bddd278956473665f585859346f4b134ff04f6"
   else
-    sha256 "5ede0cb06dfb112d9ad0060197726d34f49e14f72dc720ca2b819183fce56021"
+    sha256 "90cc687b471d8ab7afc23edf4efcf10743fdeebf9910952d42586d1e13315a08"
   end
 
   license "MIT"
