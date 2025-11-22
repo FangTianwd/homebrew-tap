4a91dc0fec5e2773f4961365c4b62d53be4151e4
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-22 23:07:02 +0800

    Update formula to use local source for testing

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -1,8 +1,8 @@
 class MdnsReflectorGo < Formula
   desc "mDNS reflector for forwarding multicast DNS packets between network interfaces"
   homepage "https://github.com/FangTianwd/mdns-reflector-go"
-  url "https://github.com/FangTianwd/mdns-reflector-go/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  url "file:///Users/jiangshengcheng/mdns_reflector_go"
+  sha256 :no_check
   license "MIT"
 
   depends_on "go" => :build
