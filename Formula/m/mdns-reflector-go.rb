763e1e040a5b8b870d0023c9fd879803f3347ec5
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-22 23:00:24 +0800

    Add mdns-reflector-go v1.0.0

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -1,25 +1,24 @@
 class MdnsReflectorGo < Formula
-    desc "Simple mDNS reflector written in golang"
-    homepage "https://github.com/robotJie/mdns_reflector_go"
-    url "https://github.com/robotJie/mdns_reflector_go/archive/refs/tags/v0.0.7.tar.gz"
-    sha256 "bc713869b11809b5fa2ac68b6516d79d4e51cd78e731cc633b67c7e681889087"
-    license "MIT"
-  
-    depends_on "go" => :build
-  
-    def install
-      system "go", "build", *std_go_args(ldflags: "-s -w"), "main.go"
-    end
+  desc "mDNS reflector for forwarding multicast DNS packets between network interfaces"
+  homepage "https://github.com/FangTianwd/mdns-reflector-go"
+  url "https://github.com/FangTianwd/mdns-reflector-go/archive/refs/tags/v#{version}.tar.gz"
+  sha256 "CHANGE_THIS_WITH_ACTUAL_SHA256"
+  license "MIT"
 
-    service do
-        run [opt_bin/"mdns-reflector-go"]
-        keep_alive true
-        log_path var/"log/mdns-reflector-go.log"
-        error_log_path var/"log/mdns-reflector-go.log"
-    end
-  
-    test do
-      assert_match "-ifaces", shell_output("#{bin}/mdns-reflector-go --help 2>&1")
-    end
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", "-o", bin/"mdns-reflector-go", "."
   end
-  
+
+  service do
+    run [opt_bin/"mdns-reflector-go"]
+    keep_alive true
+    log_path var/"log/mdns-reflector-go.log"
+    error_log_path var/"log/mdns-reflector-go-error.log"
+  end
+
+  test do
+    system "#{bin}/mdns-reflector-go", "--help"
+  end
+end
