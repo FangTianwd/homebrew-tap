3ee099a1e85f383dcbcadd95bf9ecbd4efcac2c3
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-22 23:08:13 +0800

    Fix formula syntax with proper url/sha256

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -1,8 +1,11 @@
 class MdnsReflectorGo < Formula
   desc "mDNS reflector for forwarding multicast DNS packets between network interfaces"
   homepage "https://github.com/FangTianwd/mdns-reflector-go"
-  # For local development/testing
-  head "file:///Users/jiangshengcheng/mdns_reflector_go", using: :git
+
+  # For local development/testing - replace with actual release URL when publishing
+  url "https://github.com/FangTianwd/mdns-reflector-go/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+
   license "MIT"
 
   depends_on "go" => :build
