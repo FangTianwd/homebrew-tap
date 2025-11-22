2a1bad0174380e7be074b32f8b5c5855d2a5d594
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-23 00:54:08 +0800

    feat: update Homebrew formula with correct v1.0.2 release URLs and SHA256 checksums
    - Update URL format to match make release output
    - Update ARM64 SHA256: 9825fd9d2eb0b16ce38ea488d34747a8262b0de50ff5f5355093c9860123518f
    - Update AMD64 SHA256: ef16c7a7973719b7cf86dad56a7e504a6a5bf9dd83a03e98a326472951311184
    - URLs now point to actual release artifacts from make release

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -3,11 +3,11 @@ class MdnsReflectorGo < Formula
   homepage "https://github.com/FangTianwd/mdns_reflector_go"
 
   # 使用预编译二进制文件 - 无需本地编译，安装速度极快！
-  url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.0.2/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-1.0.2.tar.gz"
+  url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.0.2/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-v1.0.2.tar.gz"
   if Hardware::CPU.arm?
-    sha256 "e31ac8c0d15f6c13e2a9d2e6aa1a4234c9680681731f33f9ecf4ab31ea8a1f8f"
+    sha256 "9825fd9d2eb0b16ce38ea488d34747a8262b0de50ff5f5355093c9860123518f"
   else
-    sha256 "PLACEHOLDER_SHA256_FOR_INTEL"  # 需要 Intel 二进制文件
+    sha256 "ef16c7a7973719b7cf86dad56a7e504a6a5bf9dd83a03e98a326472951311184"
   end
 
   license "MIT"
