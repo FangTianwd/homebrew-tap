2f346a82b632406ac606bb37f005ffbcb69b53c9
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-23 01:59:44 +0800

    feat: update to v1.0.4 binary release
    - Update download URL to v1.0.4
    - Filename: mdns-reflector-go-darwin-{arch}-v1.0.4.tar.gz
    - SHA256 will be updated once Actions build completes

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -3,7 +3,7 @@ class MdnsReflectorGo < Formula
   homepage "https://github.com/FangTianwd/mdns_reflector_go"
 
   # 使用预编译二进制文件 - 无需本地编译，安装速度极快！
-  url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.0.3/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-v1.0.3.tar.gz"
+  url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.0.4/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-v1.0.4.tar.gz"
   if Hardware::CPU.arm?
     sha256 "aba7cea65e809155009b99a8ae11b858078a2cb4cad66a2b40e8f7660dedf015"
   else
