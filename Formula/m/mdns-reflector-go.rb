c96125119c92b390547ab13e99a1356a52317194
蒋升诚 <jiangshengcheng@jwzg.com>
2025-11-23 16:04:45 +0800

    update mdns-reflector-go to v1.2.1

--- Formula/m/mdns-reflector-go.rb
+++ Formula/m/mdns-reflector-go.rb
@@ -3,7 +3,7 @@ class MdnsReflectorGo < Formula
   homepage "https://github.com/FangTianwd/mdns_reflector_go"
 
   # 使用预编译二进制文件 - 无需本地编译，安装速度极快！
-  url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.2.0/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-v1.2.0.tar.gz"
+  url "https://github.com/FangTianwd/mdns_reflector_go/releases/download/v1.2.1/mdns-reflector-go-darwin-#{Hardware::CPU.arch}-v1.2.1.tar.gz"
   if Hardware::CPU.arm?
     sha256 "e982c7e831a98e85a72ef02081e499286a3d607b2058b77870d445ab0538f185"
   else
